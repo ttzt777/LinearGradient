1656c794fd8d9cb36543d2c5e46001bb3816e9ba
tianruofengxing <tianruofengxing@163.com>
2017-09-03 01:03:48 +0800

    添加计算

---
+++ app/src/main/java/com/tt/lineargradient/LinearGradientCalculator.java
@@ -0,0 +1,100 @@
+package com.tt.lineargradient;
+
+import android.graphics.Color;
+
+public class LinearGradientCalculator {
+    private int[] mColors;
+
+    private int mStartColor;  
+    private int mEndColor;
+
+    public LinearGradientCalculator() {
+
+    }
+  
+    public LinearGradientCalculator(int startColor, int endColor) {
+        this.mStartColor = startColor;  
+        this.mEndColor = endColor;  
+    }
+
+    public LinearGradientCalculator(int[] colors) {
+        this.mColors = colors;
+    }
+
+    public void setColors(int[] colors) {
+        this.mColors = colors;
+    }
+  
+    public void setStartColor(int startColor) {  
+        this.mStartColor = startColor;  
+    }  
+  
+    public void setEndColor(int endColor) {  
+        this.mEndColor = endColor;  
+    }
+
+    public int getColor(float progress) {
+        if (mColors == null || mColors.length == 0) {
+            throw new RuntimeException("LinearGradientCalculator must have colors");
+        }
+
+        if (mColors.length == 1) {
+            return mColors[0];
+        }
+
+        int phaseCount = mColors.length - 1;
+        int phase = 0;
+
+        for (int i = 0; i < phaseCount; i++) {
+            if (progress <= (i + 1) / (float) phaseCount) {
+                phase = i;
+                break;
+            }
+        }
+
+        int startColor = mColors[phase];
+        int endColor = mColors[phase + 1];
+
+        int redStart = Color.red(startColor);
+        int greenStart = Color.green(startColor);
+        int blueStart = Color.blue(startColor);
+
+        int redEnd = Color.red(endColor);
+        int greenEnd = Color.green(endColor);
+        int blueEnd = Color.blue(endColor);
+
+        float progressInPhase = (progress - phase * 1 / (float) phaseCount) / (1 / (float) phaseCount);
+
+        int red = (int) (redStart + ((redEnd - redStart) * progressInPhase + 0.5));
+        int green = (int) (greenStart + ((greenEnd - greenStart) * progressInPhase + 0.5));
+        int blue = (int) (blueStart + ((blueEnd - blueStart) * progressInPhase + 0.5));
+
+        if (red > 255) {
+            red = 255;
+        }
+
+        if (green > 255) {
+            green = 255;
+        }
+
+        if (blue > 255) {
+            blue = 255;
+        }
+
+        return Color.argb(255, red, green, blue);
+    }
+  
+    public int getColor1(float radio) {
+        int redStart = Color.red(mStartColor);
+        int blueStart = Color.blue(mStartColor);  
+        int greenStart = Color.green(mStartColor);  
+        int redEnd = Color.red(mEndColor);  
+        int blueEnd = Color.blue(mEndColor);  
+        int greenEnd = Color.green(mEndColor);  
+  
+        int red = (int) (redStart + ((redEnd - redStart) * radio + 0.5));  
+        int greed = (int) (greenStart + ((greenEnd - greenStart) * radio + 0.5));  
+        int blue = (int) (blueStart + ((blueEnd - blueStart) * radio + 0.5));  
+        return Color.argb(255,red, greed, blue);  
+    }  
+}  
--- app/src/main/java/com/tt/lineargradient/MainActivity.java
+++ app/src/main/java/com/tt/lineargradient/MainActivity.java
@@ -1,13 +1,54 @@
 package com.tt.lineargradient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+    private static final int[] colors = new int[] {
+            0xff008000,
+            0xff00ff00,
+            0xffffff00,
+            0xffff0000
+    };
+
+    private SeekBar mSeekBar;
+    private View mDisView;
+    private TextView mDisText;
+
+    private LinearGradientCalculator colorCalcutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colorCalcutor = new LinearGradientCalculator(colors);
+
+        mSeekBar = (SeekBar) findViewById(R.id.sb_progress);
+        mDisView = findViewById(R.id.view);
+        mDisText = (TextView) findViewById(R.id.tv_dis);
+
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        float progress = i / 1000000f;
+        mDisText.setText(String.valueOf(progress));
+        mDisView.setBackgroundColor(colorCalcutor.getColor(progress));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
